c02249f7f747841b93a958777d25fde4e701569e
Levin Jian <jianzhirong@gmail.com>
2020-11-06 17:16:25 +0800

    debug into c++

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,8 @@
-import cmake_example as m
+import sys 
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../bin"))
+import example as m
 
-assert m.__version__ == '0.0.1'
-assert m.add(1, 2) == 3
+# assert m.__version__ == '0.0.1'
+assert m.add(10, 20) == 30
 assert m.subtract(1, 2) == -1
