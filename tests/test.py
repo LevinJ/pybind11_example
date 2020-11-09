79e3313b4582aa518a6f046a11b2b1ed5d8a6460
Levin Jian <jianzhirong@gmail.com>
2020-11-09 09:31:29 +0800

    add the pet example

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,13 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "../bin"))
 import example as m
+from example import Pet
 
 # assert m.__version__ == '0.0.1'
 assert m.add(10, 20) == 30
-assert m.subtract(1, 2) == -1
+# assert m.subtract(1, 2) == -1
+
+pet = m.Pet("initial name")
+pet.setName("my pet's name")
+name = pet.getName()
+print(name)
